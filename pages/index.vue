f6ff9223e95f1690ddcc2dafea0da598c5065497
Alix Ducros <alix@krlx.fr>
2023-12-20 17:37:01 +0100

    Update French translations and homepage layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,5 @@
 <template>
-    <div>
-        Hello
+    <div class="flex items-center justify-center w-screen h-screen">
         <NuxtLink to="quizz/one" class="btn">{{ $t('home.start-quizz') }}</NuxtLink>
     </div>
 </template>
