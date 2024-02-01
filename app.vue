692b0a6a6ddd45595993b6765a1c68cf25b9ced5
Alix Ducros <alix@krlx.fr>
2024-02-01 15:41:10 +0100

    homepage mostly ok

--- app.vue
+++ app.vue
@@ -5,6 +5,10 @@ const otherLocale = computed(() => {
     return locales.value.find(l => l.code !== locale.value)
 })
 
+function foldMenu () {
+    document.querySelector('#nav-menu').removeAttribute('open')
+}
+
 </script>
 
 <template>
@@ -24,18 +28,20 @@ const otherLocale = computed(() => {
                     <a href="#partners" class="text-lg">{{ $t('home.partners') }}</a>
                 </li>
                 <li class="hidden lg:block mx-4">
-                    <nuxt-link to="/map" class="text-lg">{{ $t('home.map') }}</nuxt-link>
+                    <a href="#" class="text-lg">{{ $t('home.map') }}</a>
                 </li>
                 <li class="hidden lg:block mx-4">
                     <div class="text-lg" @click="setLocale(otherLocale.code)">{{ otherLocale.name }}</div>
                 </li>
                 <li class="lg:hidden">
-                    <details class="dropdown dropdown-bottom dropdown-end">
+                    <details id="nav-menu" class="dropdown dropdown-bottom dropdown-end">
                         <summary>
                             {{ $t('home.menu') }}
                         </summary>
                         <ul class="p-2 bg-base-100 rounded-t-none dropdown-content">
-                            <li><nuxt-link to="/quizz/one" class="text-primary no-underline">{{ $t('home.start-quizz') }}</nuxt-link></li>
+                            <li @click.capture="foldMenu"><nuxt-link to="/quizz/one" class="text-primary no-underline">{{ $t('home.start-quizz') }}</nuxt-link></li>
+                            <li @click.capture="foldMenu"><a href="#partners">{{ $t('home.partners') }}</a></li>
+                            <li @click.capture="foldMenu"><a href="#">{{ $t('home.map') }}</a></li>
                         </ul>
                     </details>
                 </li>
@@ -48,78 +54,78 @@ const otherLocale = computed(() => {
             <p class="text-grey text-2xl mt-6 max-w-lg">{{ $t('home.partners-text') }}</p>
             <div class="grid xl:grid-cols-6 lg:grid-cols-4 md:grid-cols-2 gap-4 mt-12">
                 <div class="col-span-2 flex flex-row space-x-8">
-                    <img class="max-h-24 max-w-[180px]" src="/france_2030.jpg" />
-                    <div class="w-full text-sm">
+                    <a target="_blank" href="https://www.u-bordeaux.fr/universite/notre-strategie/projets-institutionnels/act-pour-un-campus-experimental"><img class="max-h-24 max-w-[180px]" src="/france_2030.jpg" /></a>
+                    <div class="w-full text-neutral-500 text-sm">
                         {{ $t('home.anr-financing') }}
                     </div>
                 </div>
-                <div><img class="max-h-24 max-w-[180px]" src="/science_societe.png" /></div>
-                <div><img class="max-h-24 max-w-[180px]" src="/univ_bordeaux.png" /></div>
-                <div><img class="max-h-24 max-w-[180px]" src="/urfist.png" /></div>
-                <div><img class="max-h-24 max-w-[180px]" src="/inria.png" /></div>
-                <div><img class="max-h-24 max-w-[180px]" src="/floirac.jpg" /></div>
-                <div><img class="max-h-24 max-w-[180px]" src="/inrae.jpg" /></div>
-                <div><img class="max-h-24 max-w-[180px]" src="/valmont.png" /></div>
-                <div><img class="max-h-24 max-w-[180px]" src="/durkheim.jpg" /></div>
-                <div><img class="max-h-24 max-w-[180px]" src="/records.png" /></div>
-                <div><img class="max-h-24 max-w-[180px]" src="/ares.jpg" /></div>
+                <div><a target="_blank" href="https://www.u-bordeaux.fr/recherche/science-et-societe"><img class="max-h-24 max-w-[180px]" src="/science_societe.png" /></a></div>
+                <div><a target="_blank" href="https://www.u-bordeaux.fr/"><img class="max-h-24 max-w-[180px]" src="/univ_bordeaux.png" /></a></div>
+                <div><a target="_blank" href="http://weburfist.univ-bordeaux.fr/"><img class="max-h-24 max-w-[180px]" src="/urfist.png" /></a></div>
+                <div><a target="_blank" href="https://www.inria.fr/fr"><img class="max-h-24 max-w-[180px]" src="/inria.png" /></a></div>
+                <div><a target="_blank" href="https://www.ville-floirac33.fr/"><img class="max-h-24 max-w-[180px]" src="/floirac.jpg" /></a></div>
+                <div><a target="_blank" href="https://www.inrae.fr/"><img class="max-h-24 max-w-[180px]" src="/inrae.jpg" /></a></div>
+                <div><a target="_blank" href="https://www.villavalmont.com/"><img class="max-h-24 max-w-[180px]" src="/valmont.png" /></a></div>
+                <div><a target="_blank" href="https://www.centreemiledurkheim.fr/"><img class="max-h-24 max-w-[180px]" src="/durkheim.jpg" /></a></div>
+                <div><a target="_blank" href="https://www.helloasso.com/associations/streetdefrecords"><img class="max-h-24 max-w-[180px]" src="/records.png" /></a></div>
+                <div><a target="_blank" href="https://ville-ares.fr/"><img class="max-h-24 max-w-[180px]" src="/ares.jpg" /></a></div>
             </div>
         </div>
         <div class="h-16 w-full bg-neutral-200"></div>
-        <div id="footer" class="py-16 px-16 md:px-28 grid gap-4 grid-cls-1 lg:grid-cols-2">
+        <div id="footer" class="py-16 px-16 md:px-28 grid gap-8 grid-cls-1 lg:grid-cols-2">
             <div class="flex flex-col">
                 <NuxtImg src="/ecodoc.png" class="w-32" />
                 <div class="divider"></div>
-                <div class="font-semibold">{{ $t('home.contact-us') }}</div>
-                <div><a href="mailto:raphaelle.bats@u-bordeaux.fr">raphaelle.bats@u-bordeaux.fr</a></div>
-                <div><a href="mailto:mathilde.garnier@u-bordeaux.fr">mathilde.garnier@u-bordeaux.fr</a></div>
+                <div class="font-semibold mb-4">{{ $t('home.contact-us') }}</div>
+                <div class="text-neutral-500"><a href="mailto:raphaelle.bats@u-bordeaux.fr">raphaelle.bats@u-bordeaux.fr</a></div>
+                <div class="text-neutral-500"><a href="mailto:mathilde.garnier@u-bordeaux.fr">mathilde.garnier@u-bordeaux.fr</a></div>
             </div>
-            <nav class="grid grid-cols-1 lg:grid-cols-3" aria-label="Liens additionnels">
+            <nav class="grid grid-cols-1 lg:grid-cols-3 gap-4" aria-label="Liens additionnels">
                 <div class="footer-links-col">
-                    <p class="uppercase font-semibold"> Partenaires internes </p>
+                    <p class="mt-4 uppercase font-semibold"> Partenaires internes </p>
                     <ul>
-                        <li>
-                            <a href="http://weburfist.univ-bordeaux.fr/" target="_blank">URFIST</a>
+                        <li class="mt-3">
+                            <a class="text-neutral-500" href="http://weburfist.univ-bordeaux.fr/" target="_blank">URFIST</a>
                         </li>
-                        <li>Service culture</li>
-                        <li>
-                            <a href="https://bibliotheques.u-bordeaux.fr/" target="_blank">Direction de la Documentation</a>
+                        <li class="mt-3">Service culture</li>
+                        <li class="mt-3">
+                            <a class="text-neutral-500" href="https://bibliotheques.u-bordeaux.fr/" target="_blank">Direction de la Documentation</a>
                         </li>
-                        <li>SAPS</li>
-                        <li>ACT</li>
+                        <li class="mt-3">SAPS</li>
+                        <li class="mt-3">ACT</li>
                     </ul>
                 </div>
                 <div class="footer-links-col">
-                    <p class="uppercase font-semibold">Partenaires externes</p>
+                    <p class="mt-4 uppercase font-semibold">Partenaires externes</p>
                     <ul>
-                        <li>
-                            <a href="https://ville-ares.fr/" target="_blank">Ville d'Arès</a>
+                        <li class="mt-3">
+                            <a  class="text-neutral-500" href="https://ville-ares.fr/" target="_blank">Ville d'Arès</a>
                         </li>
-                        <li>
-                            <a href="https://www.ville-floirac33.fr/" target="_blank">Ville de Floirac</a>
+                        <li class="mt-3">
+                            <a  class="text-neutral-500" href="https://www.ville-floirac33.fr/" target="_blank">Ville de Floirac</a>
                         </li>
-                        <li>
-                            <a href="https://www.villavalmont.com/" target="_blank">Villa Valmont</a>
+                        <li class="mt-3">
+                            <a  class="text-neutral-500" href="https://www.villavalmont.com/" target="_blank">Villa Valmont</a>
                         </li>
-                        <li>
-                            <a href="https://www.helloasso.com/associations/streetdefrecords" target="_self">Street Def Records</a>
+                        <li class="mt-3">
+                            <a  class="text-neutral-500" href="https://www.helloasso.com/associations/streetdefrecords" target="_self">Street Def Records</a>
                         </li>
                     </ul>
                 </div>
                 <div class="footer-links-col">
-                    <p class="uppercase font-semibold">Partenaires académiques</p>
+                    <p class="mt-4 uppercase font-semibold">Partenaires académiques</p>
                     <ul>
-                        <li>
-                            <a href="https://www.inria.fr/fr" target="_blank">Inria</a>
+                        <li class="mt-3">
+                            <a  class="text-neutral-500" href="https://www.inria.fr/fr" target="_blank">Inria</a>
                         </li>
-                        <li>
-                            <a href="https://www.inrae.fr/" target="_blank">INRAE</a>
+                        <li class="mt-3">
+                            <a  class="text-neutral-500" href="https://www.inrae.fr/" target="_blank">INRAE</a>
                         </li>
-                        <li>
-                            <a href="https://cote.labex.u-bordeaux.fr/Unites-partenaires/UMR-BIOGECO-BIOdiversity-GEnes-COmmunities-i294.html" target="_blank">Biogeco</a>
+                        <li class="mt-3">
+                            <a  class="text-neutral-500" href="https://cote.labex.u-bordeaux.fr/Unites-partenaires/UMR-BIOGECO-BIOdiversity-GEnes-COmmunities-i294.html" target="_blank">Biogeco</a>
                         </li>
-                        <li>
-                            <a href="https://www.centreemiledurkheim.fr/" target="_blank">Centre Emile Durkheim</a>
+                        <li class="mt-3">
+                            <a  class="text-neutral-500" href="https://www.centreemiledurkheim.fr/" target="_blank">Centre Emile Durkheim</a>
                         </li>
                     </ul>
                 </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="flex items-center justify-center w-screen h-screen">
-        <NuxtLink to="quizz/one" class="btn">{{ $t('home.start-quizz') }}</NuxtLink>
+    <div class="flex flex-row lg:space-x-20 py-16 px-16 md:px-28 w-full items-center">
+        <NuxtImg class="hidden lg:block rounded max-w-96 max-h-96" src="https://picsum.photos/512/512"/>
+        <div>
+            <h1 class="text-6xl font-bold text-primary">{{ $t('home.title') }}</h1>
+            <p class="text-xl text-neutral-500 mt-5">{{ $t('home.intro-text') }}</p>
+            <NuxtLink to="quizz/one" class="btn text-white bg-primary mt-5">{{ $t('home.cta') }}</NuxtLink>
+        </div>
     </div>
 </template>
