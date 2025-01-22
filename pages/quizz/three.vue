aada97e01a5173d4f993248357f4c73459449c1e
Alix <alix@krlx.fr>
2025-01-22 20:42:45 +0100

    Correction typo

--- pages/quizz/three.vue
+++ pages/quizz/three.vue
@@ -9,12 +9,12 @@ onMounted(() => {
 
 <template>
     <div class="h-full flex flex-col justify-between">
-        <div class="flex flex-col space-y-4 h-[440px] overflow-y-scroll pb-6 pl-0.5">
+        <div class="flex flex-col space-y-4 h-[440px] overflow-y-scroll pb-10 pl-0.5">
             <p class="font-bold">{{ $t('quizz.three.question') }}</p>
             <SingleAnswerButton toPath="/quizz/four" :label="$t('quizz.three.partners')" :value="'partners'"></SingleAnswerButton>
             <SingleAnswerButton toPath="/quizz/result" :label="$t('quizz.three.patients')"  @keyword="$emit('keyword', 'community')"></SingleAnswerButton>
             <SingleAnswerButton toPath="/quizz/result" :label="$t('quizz.three.citizens')"  @keyword="$emit('keyword', 'citizen')"></SingleAnswerButton>
-            <SingleAnswerButton toPath="/quizz/result" :label="$t('quizz.three.indegenous')"  @keyword="$emit('keyword', 'community-to-precise')"></SingleAnswerButton>
+            <SingleAnswerButton toPath="/quizz/result" :label="$t('quizz.three.indigenous')"  @keyword="$emit('keyword', 'community-to-precise')"></SingleAnswerButton>
             <SingleAnswerButton toPath="/quizz/result" :label="$t('quizz.three.others')"  @keyword="$emit('keyword', 'community-to-precise')"></SingleAnswerButton>
         </div>
         <div class="flex flex-row justify-between w-full">
