e9f2c51ba3bfa47848f0fdbe4e3bccfa80243000
Alix <alix@krlx.fr>
2025-03-11 15:40:33 +0100

    update

--- pages/quizz/three.vue
+++ pages/quizz/three.vue
@@ -12,10 +12,10 @@ onMounted(() => {
         <div class="flex flex-col space-y-4 h-[440px] overflow-y-scroll pb-10 pl-0.5">
             <p class="font-bold">{{ $t('quizz.three.question') }}</p>
             <SingleAnswerButton toPath="/quizz/four" :label="$t('quizz.three.partners')" :value="'partners'"></SingleAnswerButton>
-            <SingleAnswerButton toPath="/quizz/result" :label="$t('quizz.three.patients')"  @keyword="$emit('keyword', 'community')"></SingleAnswerButton>
-            <SingleAnswerButton toPath="/quizz/result" :label="$t('quizz.three.citizens')"  @keyword="$emit('keyword', 'citizen')"></SingleAnswerButton>
-            <SingleAnswerButton toPath="/quizz/result" :label="$t('quizz.three.indigenous')"  @keyword="$emit('keyword', 'community-to-precise')"></SingleAnswerButton>
-            <SingleAnswerButton toPath="/quizz/result" :label="$t('quizz.three.others')"  @keyword="$emit('keyword', 'community-to-precise')"></SingleAnswerButton>
+            <SingleAnswerButton toPath="/quizz/result-three" :label="$t('quizz.three.patients')"  @keyword="$emit('keyword', 'community')"></SingleAnswerButton>
+            <SingleAnswerButton toPath="/quizz/result-three" :label="$t('quizz.three.citizens')"  @keyword="$emit('keyword', 'citizen')"></SingleAnswerButton>
+            <SingleAnswerButton toPath="/quizz/result-three" :label="$t('quizz.three.indigenous')"  @keyword="$emit('keyword', 'community-to-precise')"></SingleAnswerButton>
+            <SingleAnswerButton toPath="/quizz/result-three" :label="$t('quizz.three.others')"  @keyword="$emit('keyword', 'community-to-precise')"></SingleAnswerButton>
         </div>
         <div class="flex flex-row justify-between w-full">
             <NuxtLink to="/quizz/two" class="btn btn-primary-light text-primary-medium text-base font-normal">{{ $t('misc.back') }}</NuxtLink>
